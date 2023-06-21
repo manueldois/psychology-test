c0fe3537a77843c43cb7a8f7ca21dfa6c1550078
Manuel Maldonado <manuelmaldonadosil@protonmail.com>
2023-06-21 23:09:34 +0100

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="center-h" style={{fontSize: '1.5em', display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: 30}}>
       <h1>
         Psychology test
       </h1>
 
       <h2>
-        <Link href="/questions/1" className={styles.btn} style={{marginTop: '100px'}} >
+        <Link href="/questions/1" className="btn" style={{marginTop: '100px'}} >
           Start Test
           </Link>
       </h2>
