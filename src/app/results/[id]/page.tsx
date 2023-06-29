6106c2fc8930fe1fd595b5ddf5c43d85b1139a32
Manuel Maldonado <manuelmaldonadosil@protonmail.com>
2023-06-29 20:35:26 +0100

    Dynamic error on result page

--- src/app/results/[id]/page.tsx
+++ src/app/results/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getResult } from "@/app/db";
 import Link from "next/link";
 
+export const dynamic = "error";
+
 async function Page({ params: { id } }: { params: { id: string } }) {
   const result = await getResult(id);
   return (
