b5f2ecfb2e73d1d30f959c72edb6dd5bec832428
Manuel Maldonado <manuelmaldonadosil@protonmail.com>
2023-06-29 13:12:32 +0100

    Question card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,25 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="center" style={{display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: 30}}>
-      <h1>
-        Psychology test
-      </h1>
+    <main
+      className="center"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: 30,
+      }}
+    >
+      <h1>Psychology test</h1>
 
       <h2>
-        <Link href="/questions/1" className="btn primary" style={{marginTop: '100px'}} >
+        <Link
+          href="/test"
+          className="btn primary"
+          style={{ marginTop: "100px" }}
+        >
           Start Test
-          </Link>
+        </Link>
       </h2>
     </main>
   );
--- src/components/questionCard.tsx
+++ src/components/questionCard.tsx
@@ -1,19 +1,67 @@
+"use client";
+
+import classNames from "classnames";
 import { Question } from "@/types/questionarie";
-import styles from '@/styles/questionCard.module.scss'
+import styles from "@/styles/questionCard.module.scss";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 export default function QuestionCard({
-  question: {id, question, options}
-}: {question: Question}) {
+  question: { id, question, options },
+}: {
+  question: Question;
+}) {
+  const [answers, setAnswers] = useLocalStorage("answers", "");
+
+  const setValue = (v: string) => {
+    setAnswers((ans: Record<string, string>) => ({ ...ans, [id]: v }));
+  };
+
+  const value = answers[id];
+
   return (
     <article className={styles.questionCard}>
-      <h1> Question {id} </h1>
       <h2> {question} </h2>
-
       <section className={styles.options}>
-        <button> A: {options.A} </button>
-        <button> B: {options.B} </button>
-        <button> C: {options.C} </button>
-        <button> D: {options.D} </button>
+        <label className={classNames("btn", { primary: value === "A" })}>
+          <b>[A] </b> {options.A}
+          <input
+            type="radio"
+            value={"A"}
+            checked={value === "A"}
+            name={`question-${id}`}
+            onChange={(e) => e.target.checked && setValue("A")}
+          />
+        </label>
+        <label className={classNames("btn", { primary: value === "B" })}>
+          <b>[B] </b> {options.B}
+          <input
+            type="radio"
+            value={"B"}
+            checked={value === "B"}
+            name={`question-${id}`}
+            onChange={(e) => e.target.checked && setValue("B")}
+          />
+        </label>
+        <label className={classNames("btn", { primary: value === "C" })}>
+          <b>[C] </b> {options.C}
+          <input
+            type="radio"
+            value={"C"}
+            checked={value === "C"}
+            name={`question-${id}`}
+            onChange={(e) => e.target.checked && setValue("C")}
+          />
+        </label>
+        <label className={classNames("btn", { primary: value === "D" })}>
+          <b>[D] </b> {options.D}
+          <input
+            type="radio"
+            value={"D"}
+            checked={value === "D"}
+            name={`question-${id}`}
+            onChange={(e) => e.target.checked && setValue("D")}
+          />
+        </label>
       </section>
     </article>
   );
