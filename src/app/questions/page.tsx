71380acdd5de567cdebd67f073668204e0256369
Manuel Maldonado <manuelmaldonadosil@protonmail.com>
2023-06-29 20:37:26 +0100

    force-dynamic on questions page

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { getMultipleRandom } from "../utils";
 import { getQuestionIds } from "../db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const path = getMultipleRandom(await getQuestionIds(), 4);
 
